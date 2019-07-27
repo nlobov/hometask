2dbd4a64ebbd5d67eddb3c9b574881cefbe6669b
nlobov <nlobov@gmail.com>
2019-07-27 12:55:54 +0300

    Занятие 4

---
+++ java2/chat/ChatServer.java
@@ -0,0 +1,10 @@
+package java2.chat;
+
+public class ChatServer {
+    void start(int port){
+        System.out.println("Server started at port: " + port);
+    }
+    void stop(){
+        System.out.println("Server stopped");
+    }
+}
